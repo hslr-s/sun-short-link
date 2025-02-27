00328aefbc05a45a1a67a41ec3f7a6ea2c40e668
sun <95302870@qq.com>
2025-02-27 15:37:14 +0800

    增加高级功能 1. 支持目标地址文本替换 2. 支持定义链接组，并根据定义的标识 get 获取组名字

--- main.go
+++ main.go
@@ -5,28 +5,58 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/go-yaml/yaml"
 	"github.com/gorilla/mux"
 )
 
 type Config struct {
-	Port  string `yaml:"port"`
-	Links []Link `yaml:"links"`
+	Port  string       `yaml:"port"`
+	Links []LinkConfig `yaml:"links"`
 }
 
-type Link struct {
+type URLGroup struct {
 	Name string `yaml:"name"`
-	URL  string `yaml:"url"`
-	Type int    `yaml:"type"`
+	Url  string `yaml:"url"`
+}
+
+type ReplaceGroup struct {
+	QueryName string `yaml:"query_name"`
+	Keyword   string `yaml:"keyword"`
+}
+
+type ReplaceKeyword struct {
+	Keyword string
+	Value   string
+}
+
+type LinkConfig struct {
+	Name          string          `yaml:"name"`
+	Type          int             `yaml:"type"`
+	URL           *string         `yaml:"url"`
+	URLs          *[]URLGroup     `yaml:"urls"`
+	URLsQueryName *string         `yaml:"urls_query_name"`
+	Replace       *[]ReplaceGroup `yaml:"replace"`
+}
+
+type LinkRule struct {
+	Name          string             `yaml:"name"`
+	Type          int                `yaml:"type"`
+	IsGroup       bool               `yaml:"is_group"`
+	URL           *string            `yaml:"url"`
+	URLs          *map[string]string `yaml:"urls"`
+	URLsQueryName *string            `yaml:"urls_query_name"`
+	Replace       *[]ReplaceGroup    `yaml:"replace"`
 }
 
 var (
 	configPath string
 	example    bool
 	config     Config
-	shortLinks map[string]Link
+	shortLinks map[string]LinkRule
 )
 
 func init() {
@@ -49,10 +79,11 @@ func main() {
 
 	loadConfig()
 
-	shortLinks = make(map[string]Link)
-	for _, link := range config.Links {
-		shortLinks[link.Name] = link
-	}
+	// shortLinks = make(map[string]Link)
+	// for _, link := range config.Links {
+	// 	shortLinks[link.Name] = link
+	// }
+	generateRule()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/{name:.*}", redirectShortLink)
@@ -72,7 +103,38 @@ func redirectShortLink(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, link.URL, link.Type)
+	targetUrl := ""
+	if link.IsGroup && shortLinks[name].URLs != nil {
+		queryName := *link.URLsQueryName
+		queryValue := r.URL.Query().Get(queryName)
+		if queryValue == "" {
+			http.Error(w, "Query parameter not found", http.StatusBadRequest)
+			return
+		}
+
+		if shortLinks[name].URLs != nil && (*shortLinks[name].URLs)[queryValue] == "" {
+			http.Error(w, "Query parameter not found", http.StatusBadRequest)
+			return
+		}
+
+		url := (*shortLinks[name].URLs)[queryValue]
+		if link.Replace != nil {
+			url = replaceURL(url, r.URL.Query(), *link.Replace)
+		}
+		targetUrl = url
+	} else {
+		if link.URL == nil {
+			http.Error(w, "URL not found", http.StatusBadRequest)
+			return
+		}
+		targetUrl = *link.URL
+		if link.Replace != nil {
+			url := replaceURL(*link.URL, r.URL.Query(), *link.Replace)
+			targetUrl = url
+		}
+	}
+
+	http.Redirect(w, r, targetUrl, link.Type)
 }
 
 func loadConfig() {
@@ -89,6 +151,39 @@ func loadConfig() {
 	}
 }
 
+func generateRule() {
+	shortLinks = make(map[string]LinkRule)
+	for _, clink := range config.Links {
+		rule := LinkRule{
+			Name:    clink.Name,
+			Type:    clink.Type,
+			Replace: clink.Replace,
+		}
+		if clink.URLs != nil && clink.URLsQueryName != nil {
+			rule.URLsQueryName = clink.URLsQueryName
+			rule.IsGroup = true
+			urls := make(map[string]string)
+			for _, url := range *clink.URLs {
+				urls[url.Name] = url.Url
+			}
+			rule.URLs = &urls
+		} else {
+			rule.URL = clink.URL
+		}
+		shortLinks[clink.Name] = rule
+	}
+
+	// jsonData, _ := json.Marshal(shortLinks)
+	// fmt.Println("Generate rule success!", string(jsonData))
+}
+
+func replaceURL(url string, querys url.Values, replace []ReplaceGroup) string {
+	for _, r := range replace {
+		url = strings.ReplaceAll(url, r.Keyword, querys.Get(r.QueryName))
+	}
+	return url
+}
+
 func generateExampleConfig() {
 	exampleConfig := `port: 8080
 links:
@@ -101,7 +196,6 @@ links:
 `
 	if configPath == "" {
 		configPath = "example_config.yml"
-		return
 	}
 	err := os.WriteFile(configPath, []byte(exampleConfig), 0644)
 	if err != nil {
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# Sun-Short-Link (短链接)
+# Sun-Short-Link (短链接服务 | 链接中转服务)
 
 
 ## Usage
@@ -37,3 +37,44 @@ Run
 ```sh
 docker run -p 8080:8080 -v ./sun-short-link.yml:/app/sun-short-link.yml --name sun-short-link hslr/sun-short-link
 ```
+
+### 高级功能
+
+#### 1. 支持目标链接关键字替换 示例
+
+配置文件：
+```yml
+  - name: update_log
+    url: https://xxx.top/zh_cn/update/{version}
+    replace:
+      - query_name: version_name # get参数名字
+        keyword: '{version}' # url地址替换的关键字
+    
+```
+
+请求地址：`https://link.cc/update_log?version_name=1.0.0`
+
+目标地址：`https://xxx.top/zh_cn/update/1.0.0`
+
+#### 2. 支持定义链接组
+
+此功能一般用于多语言链接组功能
+
+配置文件：
+```yml
+  - name: donate_lang
+    type: 302
+    urls_query_name: lang
+    urls:
+      - name: zh_cn
+        url: https://github.com/hslr-s/sun-panel?v=v{version}
+      - name: en
+        url: https://doc.sun-panel.top/introduce/donate.html
+    replace:
+      - query_name: version_name # get参数名字
+        keyword: '{version}' # url地址替换的关键字
+```
+
+请求地址：`https://link.cc/donate_lang?version_name=1.7&lang=zh_cn`
+
+目标地址：`https://github.com/hslr-s/sun-panel?v=v{version}`
